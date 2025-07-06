536642594d33af57b6263be4a56d0211f6b5776c
Rabbi Mathake <Rabbi.Mathake@moore.co.za>
2025-07-06 09:59:39 +0200

    added Contact Model and Interface

---
+++ Interfaces/IContact.cs
@@ -0,0 +1,14 @@
+﻿using PhonebookAPI.Models;
+
+namespace PhonebookAPI.Interfaces
+{
+    public interface IContact
+    {
+        IEnumerable<Contact> GetAll();
+        Contact? GetByPhone(string phoneNumber);
+        void Add(Contact contact);
+        void Update(Contact contact);
+        void Delete(string phoneNumber);
+        IEnumerable<Contact> Search(string query);
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace PhonebookAPI.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string? Email { get; set; }
+    }
+}
